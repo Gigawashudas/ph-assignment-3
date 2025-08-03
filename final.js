0023471815e21e77708e2428f437a8f84de4cb54
Gigawashu Das <hreevud@gmail.com>
2025-08-03 23:25:52 +0600

    problem5 done

--- final.js
+++ final.js
@@ -42,3 +42,11 @@ if (
 } else {
     console.log("Delete")
 }
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+
+var studentEmail = `${student.name}${student.roll}.${student.department}@ph.ac.bd`;
+
+console.log(studentEmail)
